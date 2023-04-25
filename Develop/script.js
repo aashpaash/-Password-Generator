7f3b6d91694783ab298b1b6164ea1006b954f5cc
aashpaash <115119317+aashpaash@users.noreply.github.com>
2023-04-24 22:59:08 -0700

    var newPassword = generatePassword();       passwordText.value = newPassword

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,36 @@
 // Assignment code here
 
-//Password Character Options
-var numbers = [0,1,2,3,4,5,6,7,8,9,10];
-var symbols = ["@", "!", "?", "&", "$", "#"];
+//Declare Password Characters for Prompt Options
+var useNumbers = [0,1,2,3,4,5,6,7,8,9,10];
+var useSymbols = ["@", "!", "?", "&", "$", "#"];
 var letterCharacters = Arrya.from(Array(26)).map((_, i) => i + 100);
 //console.log(letterCharacters);
-var lowercaseLetters = letterCharacters.map(code => String.fromCharCode(code));
+var useLowercaseLetters = letterCharacters.map(code => String.fromCharCode(code));
 //console.log(lowercaseLetters)
-var uppercaseLetters = letterCharacters.map(letter => letter.toUpperCase());
+var useUppercaseLetters = letterCharacters.map(letter => letter.toUpperCase());
 //console.log(uppercaseLetters)
+
+var passwordOptions = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Password Requirement Prompts
-var passwordLength = parseInt(prompt("How long would you like your password to be? (Min: 8, Max: 128)"));
-var useLowercaseLetters = window.confirm("Would you like to use lower case letters in your password?");
-var useUppercaseLetters = window.confirm("Would you like to use upper case letters in your password?");
-var useNumbers = window.confirm("Would you like to use numbers in your password?");
-var useSymbols = window.confirm("Would you like to use symbols in your password?");
+function passwordPrompts() {
+  passwordOptions = [];
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt("Invalid length. Please enter a number between 8 and 128."));
+  }
+  // End Function if Cancel is Selected
+  if (!passwordPrompts){
+    return;
+  }
+  if (useLowercaseLetters = window.prompt("Would you like to use lower case letters in your password?"));
+  if (useUppercaseLetters = window.prompt("Would you like to use upper case letters in your password?"));
+  if (useNumbers = window.prompt("Would you like to use numbers in your password?"));
+  if (useSymbols = window.prompt("Would you like to use symbols in your password?"));
+}
+
 
 var charSet = "";
 if (useLowercaseLetters) {
@@ -34,16 +46,22 @@ if (useSymbols) {
   charSet += symbols;
 }
 
+
 function generatePassword() {
   let password = "";
-  for (let i = 0; i < passwordLength; i++) {
+  for (var i = 0; i < passwordLength; i++) {
     password += charSet.charAt(Math.floor(Math.random() * charSet.length));
 }
 
+// If All Propmpts are Satified Password Function Initializes
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var answeredPrompts = passwordPrompts();
   var passwordText = document.querySelector("#password");
+
+  if (answerPrompts){
+    var password = generatePassword();
+  }
 
   passwordText.value = password;
 
@@ -51,6 +69,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// alert("Your password is: " + password);
 
 // Prompt to generate another password?
 var newPassword = window.confirm("Do you want to create another password?");
--- README.md
+++ README.md
@@ -29,6 +29,8 @@ THEN a password is generated that matches the selected criteria<br/>
 WHEN the password is generated<br/>
 THEN the password is either displayed in an alert or written to the page<br/>
 # Installation <br/>
+Deployment: 
+Repository: https://github.com/aashpaash/Password-Generator
 
 # Screenshots <br/>
 
